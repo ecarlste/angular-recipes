a25ff1fa6284fd4685dbf7b5a4a6925eca67d217
Erik Carlsten <ecarlsten@tendrilinc.com>
2018-02-19 10:01:54 -0700

    Added interaction between the recipe list and the recipe details

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from '../../recipe.model';
 
 @Component({
@@ -8,6 +8,7 @@ import {Recipe} from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() myRecipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
@@ -15,4 +16,7 @@ export class RecipeItemComponent implements OnInit {
     console.log('recipe is: ' + this.myRecipe);
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
