8fda1cf922b40a88891efe1f644fa2de39fa0cec
Erik Carlsten <ecarlste@gmail.com>
2018-02-23 12:48:30 -0700

    Added shopping list and auth modules to help with lazy loading

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ]
+})
+export class ShoppingListModule { }
